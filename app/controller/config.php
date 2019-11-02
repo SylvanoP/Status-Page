f8f2fe91e2840640e596a0d7c223c0842a95e27d
SylvanoP <47647452+SylvanoP@users.noreply.github.com>
2019-11-02 04:25:55 +0100

    Update config.php

--- app/controller/config.php
+++ app/controller/config.php
@@ -7,6 +7,8 @@ $db_password = '';
 
 $siteName = 'Status Page';
 
+$crone_key = 'YourCroneKey';
+
 $url = 'https://statuspage.plocic.de/';
 $cdnUrl = $url.'assets/style/';
 $picUrl = $url.'assets/images/';
